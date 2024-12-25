b49abedc3ef7fde3af6c631c154e87e97af2f901
Mithil-Goradia <mithilgoradia08@gmail.com>
2024-12-25 11:06:18 +0530

    Fixed import path for liked page

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Liked from './pages/liked';
+import Liked from './pages/Liked.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
